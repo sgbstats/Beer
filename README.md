d7c4bb69bc6fda22756ea79252b754c8f80951fd
Sebastian Bate <sebastiangbate@gmail.com>
2022-01-03 21:42:43 +0000

    Update to data

--- README.md
+++ README.md
@@ -2,4 +2,21 @@
 
 This is by ratings of all of my beers on Untappd 
 
-[Add me on Untappd](https://untappd.com/user/seb_bate_4667)
+**[Add me on Untappd](https://untappd.com/user/seb_bate_4667)**
+
+## Methods
+
+I have copied all of my data from Untappd into Excel with the Brewery, Beer, Style, ABV, IBU, My Rating, Global Rating and the Date.
+
+I have broadly grouped the beers into styles
+
+* Ale - Dark
+* Ale - Light
+* Bitter
+* Fruit
+* Lager
+* Pale Ale
+* Sour
+* Stout (inc. Porter)
+* Wheat
+
--- beer.R
+++ beer.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(readxl)
-setwd("C:/Users/sebas/Google Drive/sideprojects/beer")
 beer=read_excel("beer.xlsx")
 
 
-beer=beer%>%mutate(diff=MyRating-GlobalRating)
+beer=beer%>%mutate(diff=MyRating-GlobalRating,
+                   n=row_number())
 
 
 plot(density(beer$diff))
@@ -21,12 +21,22 @@ count=beer%>%group_by(Style)%>%tally()
 ggplot(data=beer, aes(y=MyRating, color=Style, x=Style))+geom_boxplot(width=0.5)+
   geom_text(data = count,
             aes(Style, Inf, label = n), vjust = 1)+
+  theme_bw()+
   theme(legend.position = "none")
+ggsave("Plots/Ratingbystlye.png", device = "png", height = 5.68, width = 9.4, units = "in")
+  
 
 ggplot(data=beer, aes(x=GlobalRating, y=MyRating))+
   geom_point(aes(color=Style))+
-  geom_smooth(method="lm")
+  geom_smooth(method="lm", se=F)+
+  theme_bw()
+ggsave("Plots/Ratingbyglobal.png", device = "png", height = 5.68, width = 9.4, units = "in")
 
+beer %>% ggplot(aes(x=Date, y=n))+
+  geom_line()+
+  ylab("Cumulative Beers")+
+  theme_bw()
+ggsave("Plots/Culativebeers.png", device = "png", height = 5.68, width = 9.4, units = "in")
 
-ggplot(data=beer, aes(x=IBU, y=MyRating))+geom_point()
+  
 
